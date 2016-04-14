b45a1ec86c1bd4f4a36de430d8e70303085a0b0e
phi161 <phi161@users.noreply.github.com>
2016-04-14 23:53:59 +0300

    Add Xcode's boilerplate Core Data stack code (for now)

--- Babies/ViewController.swift
+++ Babies/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    var moc: NSManagedObjectContext?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
