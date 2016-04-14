222efe32daadedd962c750c7942be25b1c410627
phi161 <phi161@users.noreply.github.com>
2016-04-15 00:19:08 +0300

    Add a tableView and prepare the owning view controller

--- Babies/ViewController.swift
+++ Babies/ViewController.swift
@@ -9,20 +9,35 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    
     var moc: NSManagedObjectContext?
+    
+    // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - UITableViewDelegate
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BabyListCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "row \(indexPath.row)"
+        cell.detailTextLabel?.text = "row \(indexPath.row)"
+        
+        return cell
+    }
 
 }
 
