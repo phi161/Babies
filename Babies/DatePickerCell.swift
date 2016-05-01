1549b9b111f7d300634e712870e1317ccbaa5b8d
phi161 <phi161@users.noreply.github.com>
2016-05-02 00:54:24 +0300

    Add a clear button and animate its appearance if the cell is expanded

--- Babies/DatePickerCell.swift
+++ Babies/DatePickerCell.swift
@@ -12,19 +12,35 @@ class DatePickerCell: UITableViewCell, UIPickerViewDelegate {
     
     @IBOutlet var datePicker: UIDatePicker!
     @IBOutlet var dateLabel: UILabel!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
+    @IBOutlet var clearButton: UIButton!
+    @IBOutlet var clearButtonTrailingConstraint: NSLayoutConstraint!
+    var isExpanded = true //
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        if selected {
+            isExpanded = !isExpanded
+            
+            if isExpanded {
+                self.clearButtonTrailingConstraint.constant = 0
+                self.dateLabel.textColor = UIColor.redColor()
+            } else {
+                self.clearButtonTrailingConstraint.constant = -64
+                self.dateLabel.textColor = UIColor.blueColor()
+            }
+            UIView.animateWithDuration(0.3) {
+                self.contentView.layoutIfNeeded()
+            }
+        }
     }
     
     @IBAction func datePickerChanged(sender: UIDatePicker) {
         self.dateLabel.text = NSDateFormatter.localizedStringFromDate(sender.date, dateStyle: .MediumStyle, timeStyle: .ShortStyle)
     }
+    
+    @IBAction func clearButtonTapped(sender: AnyObject) {
+        //
+    }
+    
 }
