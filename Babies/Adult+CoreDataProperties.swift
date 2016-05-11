68999b3b26429e0f82fe24c722c774e8cd533298
phi161 <phi161@users.noreply.github.com>
2016-05-12 01:08:22 +0300

    Add a property for storing the order of the adults

--- Babies/Adult+CoreDataProperties.swift
+++ Babies/Adult+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Adult+CoreDataProperties.swift
 //  Babies
 //
-//  Created by phi161 on 11/05/16.
+//  Created by phi161 on 12/05/16.
 //  Copyright © 2016 Stanhope Road. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -17,7 +17,8 @@ extension Adult {
     @NSManaged var contactIdentifier: String?
     @NSManaged var familyName: String?
     @NSManaged var givenName: String?
+    @NSManaged var displayOrder: NSNumber?
     @NSManaged var babies: NSSet?
-    @NSManaged var type: NSManagedObject?
+    @NSManaged var type: AdultType?
 
 }
