99d9f55fd45b350332927c78d71b86f3bbd4bc59
phi161 <phi161@users.noreply.github.com>
2016-05-13 23:39:14 +0300

    Add a custom class for AdultCell

---
+++ Babies/AdultCell.swift
@@ -0,0 +1,24 @@
+//
+//  AdultCell.swift
+//  Babies
+//
+//  Created by phi161 on 13/05/16.
+//  Copyright © 2016 Stanhope Road. All rights reserved.
+//
+
+import UIKit
+
+class AdultCell: UITableViewCell {
+
+    @IBOutlet var typeButton: UIButton!
+    @IBOutlet var contactButton: UIButton!
+
+    @IBAction func typeButtonTapped(sender: AnyObject) {
+        print(#function)
+    }
+    
+    @IBAction func contactButtonTapped(sender: AnyObject) {
+        print(#function)
+    }
+    
+}
