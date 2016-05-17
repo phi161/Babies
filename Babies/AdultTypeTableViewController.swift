a548bfbd5da1f14537c8a2ff3ba124e4bc0af5da
phi161 <phi161@users.noreply.github.com>
2016-05-17 10:14:15 +0300

    Add a UITableViewController subclass for managing adult types

---
+++ Babies/AdultTypeTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AdultTypeTableViewController.swift
+//  Babies
+//
+//  Created by phi161 on 17/05/16.
+//  Copyright © 2016 Stanhope Road. All rights reserved.
+//
+
+import UIKit
+
+class AdultTypeTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("AdultTypeCellIdentifier", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath.section) - \(indexPath.row)"
+        
+        return cell
+    }
+
+}
