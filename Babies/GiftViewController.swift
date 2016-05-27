9501834695b6691489d76e1a5bc11e16f369d3c7
phi161 <phi161@users.noreply.github.com>
2016-05-27 17:17:54 +0300

    Add initial version of gifts view controller

---
+++ Babies/GiftViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GiftViewController.swift
+//  Babies
+//
+//  Created by phi161 on 27/05/16.
+//  Copyright © 2016 Stanhope Road. All rights reserved.
+//
+
+import UIKit
+
+class GiftViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
