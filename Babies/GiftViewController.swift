eb7d2c262607695bf49c544cf9ff52bf0cc4549a
phi161 <phi161@users.noreply.github.com>
2016-05-27 18:57:45 +0300

    Populate the interface with Gift details

--- Babies/GiftViewController.swift
+++ Babies/GiftViewController.swift
@@ -13,6 +13,10 @@ protocol GiftViewControllerDelegate: class {
 }
 
 class GiftViewController: UIViewController {
+    
+    @IBOutlet var detailsTextView: UITextView!
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var priceTextField: UITextField!
 
     var gift: Gift
     weak var delegate: GiftViewControllerDelegate?
@@ -27,11 +31,15 @@ class GiftViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: #selector(doneButtonTapped(_:)))
+        
+        self.datePicker.date = gift.date!
+        self.detailsTextView.text = gift.details
+        self.priceTextField.text = gift.price?.stringValue
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,6 +49,10 @@ class GiftViewController: UIViewController {
     // MARK: - Actions
     
     @IBAction func doneButtonTapped(sender: AnyObject) {
+        gift.date = self.datePicker.date
+        gift.details = self.detailsTextView.text
+        gift.price = (self.priceTextField.text! as NSString).floatValue
+        
         self.delegate?.giftViewController(self, didFinishWithGift: gift)
     }
 
