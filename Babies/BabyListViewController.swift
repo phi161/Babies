c59af0d2552af4e873c61314f5e30dc0df26bc89
phi161 <phi161@users.noreply.github.com>
2020-01-06 22:55:51 +0200

    Add the view controller for the list of babies

---
+++ Babies/BabyListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BabyListViewController.swift
+//  Babies
+//
+//  Created by phi on 06/01/2020.
+//  Copyright © 2020 phi161. All rights reserved.
+//
+
+import UIKit
+
+class BabyListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
