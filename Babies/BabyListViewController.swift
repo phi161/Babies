af332c825e3521e0597d225dbaf609767317dd5e
phi161 <phi161@users.noreply.github.com>
2020-01-16 00:14:05 +0200

    Try out RxSwift for populating the tableview with dummy data

--- Babies/BabyListViewController.swift
+++ Babies/BabyListViewController.swift
@@ -6,12 +6,27 @@
 //  Copyright © 2020 phi161. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class BabyListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let disposeBag = DisposeBag()
+    private let provider = CoreDataBabyProvider()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+        provider.babies()
+            .map{$0.sorted{$0.lastName > $1.lastName}} // convert Set to a sorted Array
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+                cell.textLabel?.text = "\(element) @ row \(row)"
+            }
+            .disposed(by: disposeBag)
     }
 
 }
--- Babies/BabyProvider.swift
+++ Babies/BabyProvider.swift
@@ -7,17 +7,18 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol BabyProvider {
-    func babies() -> Set<Baby>
+    func babies() -> Observable<Set<Baby>>
 }
 
 struct CoreDataBabyProvider: BabyProvider {
-    func babies() -> Set<Baby> {
-        return [
-            Baby(firstName: "f1", lastName: "l1", existence: .unborn(due: Date()), gender: .girl),
-            Baby(firstName: "f2", lastName: "l2", existence: .unborn(due: Date()), gender: .boy),
-            Baby(firstName: "f3", lastName: "l3", existence: .born(birthday: Date()), gender: .unspecified)
-        ]
+    func babies() -> Observable<Set<Baby>> {
+        return Observable.just([
+                Baby(firstName: "f1", lastName: "l1", existence: .unborn(due: Date()), gender: .girl),
+                Baby(firstName: "f2", lastName: "l2", existence: .unborn(due: Date()), gender: .boy),
+                Baby(firstName: "f3", lastName: "l3", existence: .born(birthday: Date()), gender: .unspecified)
+            ])
     }
 }
